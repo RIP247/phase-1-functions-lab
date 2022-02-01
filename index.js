6522d23bfa4cc7df63f1767425f604cea71262a5
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-01 10:04:03 -0500

    commit tree 0b32872b1aea843332e940aaf734b8690b07c62d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+let HQ = 42
+let feet = 12
+
+function distanceFromHqInBlocks(blocks) {
+   if ( HQ > blocks) {
+       return (blocks - HQ) * -1
+   } else if ( blocks > HQ) {
+       return (HQ - blocks) * -1
+   }
+ return distanceFromHqInBlocks
+  };
+  
+
+  function distanceFromHqInFeet(blocks) {
+   return distanceFromHqInBlocks(blocks) * 264
+  };
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+  };
+
+
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+             return 0;
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02; 
+    } else if (distanceTravelledInFeet(start, destination) <= 2500) {
+            return 25
+    } else {
+            return 'cannot travel that far';
+        }
+        
+    }
+    
+
